a42c4aa35c4d2d33b6b306d2688d8560e24ed37d
Никита Мелузов <nikita.meluzov76@gmail.com>
2022-03-21 08:08:59 +0300

    Добавьте файлы проекта.

---
+++ AddZap.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zachet2
+{
+    /// <summary>
+    /// Логика взаимодействия для AddZap.xaml
+    /// </summary>
+    public partial class AddZap : Page
+    {
+        private Service addzap;
+        ClientService clientserv = new ClientService();
+        bool flag;
+        public AddZap(Service adzap)
+        {
+            InitializeComponent();
+            addzap = adzap;
+            TBServiceName.Text = addzap.Title;
+            string fio = "";
+            List<Client> C = BaseClass.Base.Client.ToList();
+
+            for (int i = 0; i < C.Count; i++)
+            {
+                fio = C[i].LastName + " " + C[i].FirstName + " " + C[i].Patronymic;
+                CBClients.Items.Add(fio);
+            }
+        }
+
+        private void TextBlock_Loaded(object sender, RoutedEventArgs e)
+        {
+            TextBlock tb = (TextBlock)sender;
+            int index = addzap.ID;
+            List<Service> DT = BaseClass.Base.Service.Where(x => x.ID == index).ToList();
+            int minutes = 0;
+            foreach (Service s in DT)
+            {
+                minutes = s.DurationInSeconds / 60;
+            }
+            tb.Text = "Продолжительность курса - " + minutes + " минут";
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FrameClass.FrameMain.Navigate(new SerList());
+        }
+
+        private void AddReg_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int starttime=0;
+                int clientid = CBClients.SelectedIndex + 1;
+                int serviceid = addzap.ID;
+
+                Regex r = new Regex("^[0-1]{1}[0-9]{1}:[0-5]{1}[0-9]{1}$");
+                if (r.IsMatch(TBStartTime.Text) == false) 
+                {
+                    starttime = 0;
+                }
+                else { starttime += 1; }
+                r = new Regex("^[2]{1}[0-3]{1}:[0-5]{1}[0-9]{1}$");
+                if (r.IsMatch(TBStartTime.Text) == false) 
+                {
+                    starttime = 0;
+                }
+                else { starttime += 1; }
+                if (starttime == 0)
+                {
+                    MessageBox.Show("Некорректное время", "Запись клиента");
+                }
+                if (starttime >= 1)
+                {
+                    DateTime dt = Convert.ToDateTime(DPDate.SelectedDate);                    
+                    dt = dt.Add(TimeSpan.Parse(TBStartTime.Text));
+                    if (dt < DateTime.Now)
+                    {
+                        MessageBox.Show("Некорректная дата!", "Запись клиента");
+                    }
+                    else
+                    {
+                        flag = true;
+                    }
+                    if (flag)
+                    {
+                        clientserv.ClientID = clientid; 
+                        clientserv.ServiceID = serviceid; 
+                        clientserv.StartTime = dt;
+                        BaseClass.Base.ClientService.Add(clientserv);
+                        BaseClass.Base.SaveChanges();
+                        MessageBox.Show("Данные записаны!", "Запись клиента");
+                        FrameClass.FrameMain.Navigate(new SerList());
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Запись не добавлена!", "Запись клиента");
+            }
+        }
+    }
+}
---
+++ SerList.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zachet2
+{
+    /// <summary>
+    /// Логика взаимодействия для SerList.xaml
+    /// </summary>
+    public partial class SerList : Page
+    {
+        List<Service> services = BaseClass.Base.Service.ToList();
+        public SerList()
+        {
+            InitializeComponent();
+            
+            LVServices.ItemsSource = services;
+        }
+        private void TextBlock_Loaded(object sender, RoutedEventArgs e)
+        {
+            TextBlock tb = (TextBlock)sender;
+            int index = Convert.ToInt32(tb.Uid);
+            List<Service> TC = BaseClass.Base.Service.Where(x => x.ID == index).ToList();
+            string str = "";
+            foreach (Service item in TC)
+            {
+                str += item.Title;
+            }
+            tb.Text = str.Substring(0);
+
+        }
+
+        private void TextBlock_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            TextBlock tb = (TextBlock)sender;
+            int index = Convert.ToInt32(tb.Uid);
+            List<Service> TC = BaseClass.Base.Service.Where(x => x.ID == index).ToList();
+            string str = "";
+            foreach (Service item in TC)
+            {
+                if (item.Discount != 0)
+                {
+
+                    int cs = Convert.ToInt32(item.Cost) - Convert.ToInt32(Convert.ToDouble(item.Cost) * Convert.ToDouble(item.Discount));
+                    str += cs;
+                }
+                else
+                {
+                    int cs = Convert.ToInt32(item.Cost);
+                    str += cs;
+                }
+            }
+            tb.Text = "Цена:" + str.Substring(0) + " руб";
+        }
+
+        private void TextBlock_Loaded_2(object sender, RoutedEventArgs e)
+        {
+
+            TextBlock tb = (TextBlock)sender;
+            int index = Convert.ToInt32(tb.Uid);
+            List<Service> TC = BaseClass.Base.Service.Where(x => x.ID == index).ToList();
+            string str = "";
+            foreach (Service item in TC)
+            {
+
+                int time = item.DurationInSeconds / 60;
+                str += time;
+            }
+            tb.Text = str.Substring(0) + " минут";
+        }
+
+        private void TextBlock_Loaded_3(object sender, RoutedEventArgs e)
+        {
+
+            TextBlock tb = (TextBlock)sender;
+            int index = Convert.ToInt32(tb.Uid);
+            List<Service> TC = BaseClass.Base.Service.Where(x => x.ID == index).ToList();
+            string str = "";
+            foreach (Service item in TC)
+            {
+                double dis = Convert.ToDouble(item.Discount) * 100;
+                str += dis;
+            }
+            tb.Text = "Скидка: " + str.Substring(0) + "%";
+        }
+
+
+        private void Del_Click(object sender, RoutedEventArgs e)
+        {
+            Button B = (Button)sender;
+            int ind = Convert.ToInt32(B.Uid);
+            Service SerDelete = BaseClass.Base.Service.FirstOrDefault(y => y.ID == ind);
+            BaseClass.Base.Service.Remove(SerDelete);
+            BaseClass.Base.SaveChanges();
+            MessageBox.Show("Запись удалена");
+            FrameClass.FrameMain.Navigate(new SerList());
+        }
+        
+
+        private void zapis_Click(object sender, RoutedEventArgs e)
+        {
+            Button B = (Button)sender;
+            int id = Convert.ToInt32(B.Uid);
+            Service adzap = BaseClass.Base.Service.FirstOrDefault(x => x.ID == id);            
+            FrameClass.FrameMain.Navigate(new AddZap(adzap));
+        }
+    }
+}
